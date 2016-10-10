ca861adf3bce7bceeea3debc39949ef2393fcb3a
nagasudhirpulla <nagasudhirpulla@gmail.com>
2016-10-10 19:45:52 +0530

    started boiler plate for line reactor creation

---
+++ project/views/line_reactor.js
@@ -0,0 +1,14 @@
+/** Function call from the UI **/
+function createLineReactors() {
+    createLineReactor(1);
+}
+
+/** setup for line reactor creation **/
+function createLineReactor(i) {
+
+}
+
+/** create a line reactor from api **/
+function forceCreateALineReactor(voltage, end1SSName, end2SSName, lineNumber, lineOwnerName, end1SSOwnerName, end2SSOwnerName, km, conductor_type_Name, sil, noLoadMVar, end1LRMvar, end1IsSwitchable, end2LRMvar, end2IsSwitchable, iter, callback) {
+
+}
